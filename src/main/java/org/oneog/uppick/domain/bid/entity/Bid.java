a4aa9720c0afef78c6fad3a59a7b1781438f00a2
kimseokjun <k03s13j@naver.com>
2025-10-16 14:53:54 +0900

    refactor : 경매 Entity colum name 넣기, enum 수정

--- src/main/java/org/oneog/uppick/domain/bid/entity/Bid.java
+++ src/main/java/org/oneog/uppick/domain/bid/entity/Bid.java
@@ -2,42 +2,56 @@ package org.oneog.uppick.domain.bid.entity;
 
 import java.time.LocalDateTime;
 
+import org.oneog.uppick.domain.bid.enums.Status;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Bid {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(name = "product_id", nullable = false)
 	private Long productId;
 
-	@Column(nullable = true)
+	@Column(name = "current_price", nullable = true)
 	private Long currentPrice; //현재 입찰가
 
-	@Column(nullable = false)
+	@Column(name = "min_price", nullable = false)
 	private Long minPrice; //최소 입찰가
 
-	@Column(nullable = false)
-	private Enum status;
+	@Column(name = "status", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private Status status;
 
-	@Column(nullable = false, updatable = false)
-	private LocalDateTime startAt = LocalDateTime.now();
+	@CreatedDate
+	@Column(name = "start_at", nullable = false, updatable = false)
+	private LocalDateTime startAt;
 
-	@Column(nullable = false)
+	@Column(name = "end_at", nullable = false)
 	private LocalDateTime endAt;
 
 	@Builder
-	private Bid(Long productId, Long currentPrice, Long minPrice, Enum status, LocalDateTime endAt) {
+	private Bid(Long productId, Long currentPrice, Long minPrice, Status status, LocalDateTime endAt) {
 		this.productId = productId;
 		this.currentPrice = currentPrice;
 		this.minPrice = minPrice;
