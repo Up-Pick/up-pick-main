68ffd6b5e61bcdfe7e29e203f6328703562bef95
abko <abko121115@gmail.com>
2025-10-16 07:42:40 +0000

    feat: 알림 도메인 뼈대 구현

---
+++ src/main/java/org/oneog/uppick/domain/notification/service/NotificationExternalService.java
@@ -0,0 +1,8 @@
+package org.oneog.uppick.domain.notification.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationExternalService implements NotificationExternalServiceApi {
+
+}
