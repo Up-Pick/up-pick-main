1fecd3af69264b4a6e0230a2bff8d6f20d1ea880
kimseokjun <k03s13j@naver.com>
2025-10-16 16:18:15 +0900

    refactor : BiddingDetail 잘못된 클래스 제거

--- src/main/java/org/oneog/uppick/domain/auction/mapper/BiddingDetailMapper.java
+++
@@ -1,7 +0,0 @@
-package org.oneog.uppick.domain.auction.mapper;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class BiddingDetailMapper {
-}
--- src/main/java/org/oneog/uppick/domain/auction/service/BiddingExternalServiceApi.java
+++
@@ -1,4 +0,0 @@
-package org.oneog.uppick.domain.auction.service;
-
-public interface BiddingExternalServiceApi {
-}
