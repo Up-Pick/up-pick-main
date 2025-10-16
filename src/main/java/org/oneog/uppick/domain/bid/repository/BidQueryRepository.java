034adff259eae8a81ce7f18920e07baaf2039735
kimseokjun <k03s13j@naver.com>
2025-10-16 15:02:06 +0900

    refactor : QueryRepository에 JPAQueryFactor 추가

--- src/main/java/org/oneog/uppick/domain/bid/repository/BidQueryRepository.java
+++ src/main/java/org/oneog/uppick/domain/bid/repository/BidQueryRepository.java
@@ -2,9 +2,13 @@ package org.oneog.uppick.domain.bid.repository;
 
 import org.springframework.stereotype.Repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
 import lombok.RequiredArgsConstructor;
 
 @Repository
 @RequiredArgsConstructor
 public class BidQueryRepository {
+
+	private final JPAQueryFactory jpaQueryFactory;
 }
