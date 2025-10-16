c8f99e9d3c3d4214d4bb75fc5a35ac96ff70708e
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-10-16 15:36:53 +0900

    feat: 판매 상품 등록 API 구현

--- src/main/java/org/oneog/uppick/domain/product/controller/ProductController.java
+++ src/main/java/org/oneog/uppick/domain/product/controller/ProductController.java
@@ -1,13 +1,31 @@
 package org.oneog.uppick.domain.product.controller;
 
+import org.oneog.uppick.common.dto.AuthMember;
+import org.oneog.uppick.common.dto.GlobalApiResponse;
+import org.oneog.uppick.domain.product.dto.request.ProductRegisterRequest;
 import org.oneog.uppick.domain.product.service.ProductInternalService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/products")
 public class ProductController {
 
 	private final ProductInternalService productInternalService;
+
+	@PostMapping
+	public GlobalApiResponse<Object> registerProduct(
+		@Valid @RequestBody ProductRegisterRequest request,
+		@AuthenticationPrincipal AuthMember authMember) {
+
+		productInternalService.registerProduct(request, authMember.getMemberId());
+		return GlobalApiResponse.ok(null);
+	}
 }
--- src/main/java/org/oneog/uppick/domain/product/service/ProductInternalService.java
+++ src/main/java/org/oneog/uppick/domain/product/service/ProductInternalService.java
@@ -1,15 +1,36 @@
 package org.oneog.uppick.domain.product.service;
 
+import org.oneog.uppick.domain.auction.service.AuctionExternalServiceApi;
+import org.oneog.uppick.domain.product.dto.request.ProductRegisterRequest;
+import org.oneog.uppick.domain.product.entity.Product;
+import org.oneog.uppick.domain.product.mapper.ProductMapper;
 import org.oneog.uppick.domain.product.repository.ProductQueryRepository;
 import org.oneog.uppick.domain.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductInternalService {
 
+	// ***** Product Domain ***** //
 	private final ProductRepository productRepository;
 	private final ProductQueryRepository productQueryRepository;
+	private final ProductMapper productMapper;
+
+	// ****** External Domain API ***** //
+	private final AuctionExternalServiceApi auctionExternalServiceApi;
+
+	@Transactional
+	public void registerProduct(ProductRegisterRequest request, Long registerId) {
+
+		Product product = productMapper.registerToEntity(request, registerId);
+
+		// 상품 및 경매 등록
+		productRepository.save(product);
+		auctionExternalServiceApi.registerAuction(product.getId(), request.getStartBid(), request.getEndAt());
+	}
 }
