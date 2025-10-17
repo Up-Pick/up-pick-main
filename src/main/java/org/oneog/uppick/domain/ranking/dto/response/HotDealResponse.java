2d1d438f035cbe145e0d5abdef8917228c51751b
soo59599 <sooh59599@gmail.com>
2025-10-17 23:04:20 +0900

    feat: 핫딜랭킹 조회 구현 및 테스트코드 작성

---
+++ src/main/java/org/oneog/uppick/domain/ranking/dto/response/HotDealResponse.java
@@ -0,0 +1,15 @@
+package org.oneog.uppick.domain.ranking.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class HotDealResponse {
+
+	private final Integer rank;
+	private final Long productId;
+	private final String productName;
+	private final String productImage;
+
+}
--- src/main/java/org/oneog/uppick/domain/ranking/repository/HotDealRepository.java
+++ src/main/java/org/oneog/uppick/domain/ranking/repository/HotDealRepository.java
@@ -1,7 +1,12 @@
 package org.oneog.uppick.domain.ranking.repository;
 
+import java.util.List;
+
 import org.oneog.uppick.domain.ranking.entity.HotDeal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HotDealRepository extends JpaRepository<HotDeal, Long> {
+
+	List<HotDeal> findAllByOrderByRankNoAsc();
+
 }
