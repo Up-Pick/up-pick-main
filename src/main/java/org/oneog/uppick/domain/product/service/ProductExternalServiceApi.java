a2a3e9df6b4da28328f7c62c25cc5261915195a7
kimseokjun <k03s13j@naver.com>
2025-10-17 14:21:48 +0900

    feat : 잘못 요청한 메서드 지우기

--- src/main/java/org/oneog/uppick/domain/product/service/ProductExternalServiceApi.java
+++ src/main/java/org/oneog/uppick/domain/product/service/ProductExternalServiceApi.java
@@ -1,13 +1,5 @@
 package org.oneog.uppick.domain.product.service;
 
 public interface ProductExternalServiceApi {
-	/**
-	 * 상품 ID를 통해 판매자(memberId)를 조회하는 메서드.
-	 *
-	 * @param productId 상품 ID
-	 * @return 판매자 회원 ID
-	 */
-	default Long findByProdcutId(Long productId) {
-		return null;
-	}
+
 }
