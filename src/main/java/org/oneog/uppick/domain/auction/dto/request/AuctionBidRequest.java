dbbdb0cf161574273d7053223e1587ecf7779d32
kimseokjun <k03s13j@naver.com>
2025-10-17 09:43:01 +0900

    refactor :  @Positve를 사용해서 양수만 받을수 있도록 수정

--- src/main/java/org/oneog/uppick/domain/auction/dto/request/AuctionBidRequest.java
+++ src/main/java/org/oneog/uppick/domain/auction/dto/request/AuctionBidRequest.java
@@ -1,6 +1,7 @@
 package org.oneog.uppick.domain.auction.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,5 +10,6 @@ import lombok.Getter;
 public class AuctionBidRequest {
 
 	@NotNull(message = "입찰가를 비울 수 없습니다.")
+	@Positive
 	private Long biddingPrice;
 }
