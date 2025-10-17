b89ca692a27612b5c8df604d19cffbdca21603d9
baek_do_hyeon <ehgus6666@naver.com>
2025-10-18 00:18:52 +0900

    test: member 로그인 기능 테스트 추가

--- src/main/java/org/oneog/uppick/domain/auth/dto/request/LoginRequest.java
+++ src/main/java/org/oneog/uppick/domain/auth/dto/request/LoginRequest.java
@@ -2,9 +2,11 @@ package org.oneog.uppick.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class LoginRequest {
 	@NotBlank(message = "이메일은 필수 입력값입니다.")
 	@Email(message = "이메일 형식이 올바르지 않습니다.")
