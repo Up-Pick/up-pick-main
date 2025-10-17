43a10b9bc239d9d08732a612b1a5f95f5a7421ef
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-10-17 10:10:35 +0900

    test: 상품 간단 정보 조회 Test 구현

---
+++ src/main/java/org/oneog/uppick/domain/product/dto/response/ProductSimpleInfoResponse.java
@@ -0,0 +1,16 @@
+package org.oneog.uppick.domain.product.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+public class ProductSimpleInfoResponse {
+	private String name;
+	private String image;
+}
