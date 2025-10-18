0d776b09c89cdb38b8cc2827d73f1b0c8647325f
baek_do_hyeon <ehgus6666@naver.com>
2025-10-18 21:05:43 +0900

    refactor: member request notnull 적용

--- src/main/java/org/oneog/uppick/domain/member/dto/request/CreditChargeRequest.java
+++ src/main/java/org/oneog/uppick/domain/member/dto/request/CreditChargeRequest.java
@@ -1,5 +1,6 @@
 package org.oneog.uppick.domain.member.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,5 +10,6 @@ import lombok.Getter;
 public class CreditChargeRequest {
 
 	@Positive(message = "충전 금액은 0보다 커야 합니다.")//양수인지 확인
+	@NotNull(message = "충전 금액을 입력해야 합니다.")
 	private Long amount;
 }
