9bb047b33aab5aaa125359898509466af5033317
abko <abko121115@gmail.com>
2025-10-20 01:24:44 +0000

    chore: S3Config 및 환경설정에서 AWS 자격증명 관련 설정 제거

--- src/main/java/org/oneog/uppick/common/config/S3Config.java
+++ src/main/java/org/oneog/uppick/common/config/S3Config.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -14,22 +12,13 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Profile({"dev", "prod"})
 public class S3Config {
 
-	@Value("${spring.cloud.aws.credentials.access-key}")
-	private String accessKey;
-
-	@Value("${spring.cloud.aws.credentials.secret-key}")
-	private String secretKey;
-
 	@Value("${spring.cloud.aws.region.static}")
 	private String region;
 
 	@Bean
 	public S3Client s3Client() {
-		AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-
 		return S3Client.builder()
 			.region(Region.of(region))
-			.credentialsProvider(StaticCredentialsProvider.create(credentials))
 			.build();
 	}
 }
