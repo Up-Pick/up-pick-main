c3b981f9d47250ec8062d567b323a946d6119245
soo59599 <sooh59599@gmail.com>
2025-10-20 20:08:18 +0900

    docs: 인기 검색어 조회 api rest docs 용 테스트 컨트롤러 작성

---
+++ src/test/java/org/oneog/uppick/domain/ranking/controller/RankingControllerTest.java
@@ -0,0 +1,63 @@
+package org.oneog.uppick.domain.ranking.controller;
+
+import static org.mockito.BDDMockito.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.oneog.uppick.domain.ranking.dto.response.HotKeywordResponse;
+import org.oneog.uppick.domain.ranking.service.RankingInternalService;
+import org.oneog.uppick.support.RestDocsBase;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+@WebMvcTest(RankingController.class)
+public class RankingControllerTest extends RestDocsBase {
+
+	@MockitoBean
+	private RankingInternalService rankingInternalService;
+
+	@Test
+	@DisplayName("인기 검색어 조회 API를 Rest Docs 로 문서화한다")
+	void documentGetHotKeywords() throws Exception {
+		List<HotKeywordResponse> responses = List.of(
+			new HotKeywordResponse("유영관", 1),
+			new HotKeywordResponse("정직한", 2),
+			new HotKeywordResponse("김석준", 3),
+			new HotKeywordResponse("백도현", 4),
+			new HotKeywordResponse("박수현", 5),
+			new HotKeywordResponse("김선용", 6),
+			new HotKeywordResponse("박성규", 7),
+			new HotKeywordResponse("안형욱", 8),
+			new HotKeywordResponse("현석훈", 9),
+			new HotKeywordResponse("최원빈", 10)
+		);
+
+		given(rankingInternalService.getHotKeywords()).willReturn(responses);
+
+		mockMvc.perform(
+				get("/api/v1/rankings/search-keywords")
+					.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("success").value(true))
+			.andExpect(jsonPath("message").value("요청에 성공했습니다."))
+			.andExpect(jsonPath("data").isArray())
+			.andDo(
+				document(
+					"ranking-getHotKeywords",
+					responseFields(
+						fieldWithPath("success").type(JsonFieldType.BOOLEAN).description("요청 성공 여부"),
+						fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+						fieldWithPath("data[]").type(JsonFieldType.ARRAY).description("인기 검색어 목록"),
+						fieldWithPath("data[].keyword").type(JsonFieldType.STRING).description("검색어"),
+						fieldWithPath("data[].rankNo").type(JsonFieldType.NUMBER).description("순위"))));
+	}
+}
+
