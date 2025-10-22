3e5f23d166b0680113f725ab320bed3b4bc12a2d
abko <abko121115@gmail.com>
2025-10-22 02:32:54 +0000

    fix: UpPickCommonAutoConfiguration 패키지 경로 수정

--- uppick-common/src/main/java/org/oneog/uppick/common/config/UpPickCommonAutoConfiguration.java
+++ uppick-common/src/main/java/org/oneog/uppick/common/config/UpPickCommonAutoConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "org.oneog.uppickcommon")
+@ComponentScan(basePackages = "org.oneog.uppick.common")
 public class UpPickCommonAutoConfiguration {
 
 }
