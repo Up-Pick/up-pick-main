1058a47fab6326aea88645bdd0e23daec9e67549
abko <abko121115@gmail.com>
2025-10-23 08:00:11 +0000

    fix: 보안 관련 설정 제거 및 SecurityConfig 클래스 삭제

--- uppick-api-gateway/src/main/java/org/oneog/uppick/gateway/ApiGatewayApplication.java
+++ uppick-api-gateway/src/main/java/org/oneog/uppick/gateway/ApiGatewayApplication.java
@@ -2,8 +2,9 @@ package org.oneog.uppick.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
