73ab85e0187f7bfea18a1a703ec7137776cc48d8
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-10-24 15:15:17 +0900

    feat: Product 테이블 Category 비정규화 적용

---
+++ uppick-product-service/src/main/java/org/oneog/uppick/product/domain/category/service/CategoryExternalService.java
@@ -0,0 +1,30 @@
+package org.oneog.uppick.product.domain.category.service;
+
+import org.oneog.uppick.common.exception.BusinessException;
+import org.oneog.uppick.product.domain.category.dto.response.CategoryInfoResponse;
+import org.oneog.uppick.product.domain.category.entity.Category;
+import org.oneog.uppick.product.domain.category.exception.CategoryErrorCode;
+import org.oneog.uppick.product.domain.category.mapper.CategoryMapper;
+import org.oneog.uppick.product.domain.category.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryExternalService implements CategoryExternalServiceApi {
+
+	private final CategoryRepository categoryRepository;
+	private final CategoryMapper categoryMapper;
+
+	@Override
+	public CategoryInfoResponse getCategoriesByCategoryId(Long categoryId) {
+		Category category = findCategoryByIdOrElseThrow(categoryId);
+		return categoryMapper.toCategoryInfoResponse(category);
+	}
+
+	private Category findCategoryByIdOrElseThrow(Long categoryId) {
+		return categoryRepository.findById(categoryId)
+			.orElseThrow(() -> new BusinessException(CategoryErrorCode.CATEGORY_NOT_FOUND));
+	}
+}
