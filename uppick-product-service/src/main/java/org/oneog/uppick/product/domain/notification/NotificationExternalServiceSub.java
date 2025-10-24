c97581ff443cfda145c4e5f6d52232da7ed27cdb
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-10-24 19:04:01 +0900

    feat: Product 도메인의 Member, Notification External 임시 구현체 추가

---
+++ uppick-product-service/src/main/java/org/oneog/uppick/product/domain/notification/NotificationExternalServiceSub.java
@@ -0,0 +1,12 @@
+package org.oneog.uppick.product.domain.notification;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationExternalServiceSub implements NotificationExternalService {
+
+	@Override
+	public void sendNotification(Long sellerId, NotificationType bid, String string, String string2) {
+
+	}
+}
