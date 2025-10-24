9e7f174cbe36c3d87fd506ed6c7f828caf9ad215
soo59599 <sooh59599@gmail.com>
2025-10-24 15:05:49 +0900

    feat: 핫딜제거

--- uppick-main-service/src/main/java/org/oneog/uppick/domain/ranking/controller/RankingController.java
+++ uppick-main-service/src/main/java/org/oneog/uppick/domain/ranking/controller/RankingController.java
@@ -3,7 +3,6 @@ package org.oneog.uppick.domain.ranking.controller;
 import java.util.List;
 
 import org.oneog.uppick.common.dto.GlobalApiResponse;
-import org.oneog.uppick.domain.ranking.dto.response.HotDealResponse;
 import org.oneog.uppick.domain.ranking.dto.response.HotKeywordResponse;
 import org.oneog.uppick.domain.ranking.service.RankingInternalService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +17,6 @@ import lombok.RequiredArgsConstructor;
 public class RankingController {
 
 	private final RankingInternalService rankingInternalService;
-
-	@GetMapping("/hot-deals")
-	public GlobalApiResponse<List<HotDealResponse>> getHotDeals() {
-		List<HotDealResponse> responses = rankingInternalService.getHotDeals();
-		return GlobalApiResponse.ok(responses);
-	}
 
 	@GetMapping("search-keywords")
 	public GlobalApiResponse<List<HotKeywordResponse>> getHotKeywords() {
