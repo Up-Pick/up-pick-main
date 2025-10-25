3645b23826ed84a05cfff520174bccf7f8c13aea
abko <abko121115@gmail.com>
2025-10-25 08:38:27 +0000

    feat: 회원 관련 엔티티 및 서비스 삭제

--- uppick-product-service/src/main/java/org/oneog/uppick/product/domain/member/service/MemberExternalService.java
+++
@@ -1,7 +0,0 @@
-package org.oneog.uppick.product.domain.member.service;
-
-public interface MemberExternalService {
-    void updateMemberCredit(long memberId, long l);
-
-    void registerPurchaseDetail(Long auctionId, Long buyerId, Long productId, Long price);
-}
--- uppick-product-service/src/main/java/org/oneog/uppick/product/domain/notification/NotificationExternalService.java
+++
@@ -1,9 +0,0 @@
-package org.oneog.uppick.product.domain.notification;
-
-import org.oneog.uppick.product.domain.notification.enums.NotificationType;
-
-public interface NotificationExternalService {
-
-	void sendNotification(Long sellerId, NotificationType bid, String string, String string2);
-
-}
