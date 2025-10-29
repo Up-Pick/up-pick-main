9d9f5e33a0cf3b40e202c06b955b224edb96b1c0
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-10-29 10:31:57 +0900

    feat: elasticsearch 의존성 및 환경설정 적용

---
+++ uppick-auction-service/src/main/java/org/oneog/uppick/auction/common/config/ElasticsearchConfig.java
@@ -0,0 +1,10 @@
+package org.oneog.uppick.auction.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class ElasticsearchConfig {
+
+}
