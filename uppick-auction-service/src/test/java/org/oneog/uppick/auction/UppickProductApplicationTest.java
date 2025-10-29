582c775dcfcf18b0bb8aef87af31eb5337bb8aaf
abko <abko121115@gmail.com>
2025-10-29 07:04:59 +0000

    test: @Disabled 어노테이션 추가로 테스트 비활성화

--- uppick-auction-service/src/test/java/org/oneog/uppick/auction/UppickProductApplicationTest.java
+++ uppick-auction-service/src/test/java/org/oneog/uppick/auction/UppickProductApplicationTest.java
@@ -1,15 +1,12 @@
 package org.oneog.uppick.auction;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@Disabled
 public class UppickProductApplicationTest {
-
-    @MockitoBean
-    private RedissonClient redissonClient;
 
     @Test
     void contextLoads() {
