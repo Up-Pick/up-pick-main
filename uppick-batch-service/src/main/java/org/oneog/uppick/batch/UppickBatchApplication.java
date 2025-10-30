86470ceedd2bb5653186e915258dd5fc6076ab1f
baek_do_hyeon <ehgus6666@naver.com>
2025-10-31 00:04:37 +0900

    Feign, QueryDSL, Scheduling 설정 추가

--- uppick-batch-service/src/main/java/org/oneog/uppick/batch/UppickBatchApplication.java
+++ uppick-batch-service/src/main/java/org/oneog/uppick/batch/UppickBatchApplication.java
@@ -2,10 +2,8 @@ package org.oneog.uppick.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 
 @SpringBootApplication
-@EnableBatchProcessing
 public class UppickBatchApplication {
 
     public static void main(String[] args) {
