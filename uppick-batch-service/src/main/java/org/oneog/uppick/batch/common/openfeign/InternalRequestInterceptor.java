c9d1e874e6bebfc75888f5608859bc6e226b75a9
baek_do_hyeon <ehgus6666@naver.com>
2025-10-31 00:04:16 +0900

    InternalRequestInterceptor로 서비스 간 인증 구현

---
+++ uppick-batch-service/src/main/java/org/oneog/uppick/batch/common/openfeign/InternalRequestInterceptor.java
@@ -0,0 +1,27 @@
+package org.oneog.uppick.batch.common.openfeign;
+
+import org.oneog.uppick.common.constants.AuthConstant;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+@Component
+public class InternalRequestInterceptor implements RequestInterceptor {
+
+	private final String internalApiSecret;
+
+	public InternalRequestInterceptor(@Value("${internal.api.secret}")
+	String internalApiSecret) {
+
+		this.internalApiSecret = internalApiSecret;
+	}
+
+	@Override
+	public void apply(RequestTemplate template) {
+
+		template.header(AuthConstant.INTERNAL_AUTH_HEADER, internalApiSecret);
+	}
+
+}
