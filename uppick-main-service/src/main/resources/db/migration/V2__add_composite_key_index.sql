af09f4e499ff9f7340e37f552714e50cb4eec2a5
2TechSide <161797001+TwoTechSide@users.noreply.github.com>
2025-10-30 16:32:25 +0900

    feat: 테이블명 오류 수정

--- uppick-main-service/src/main/resources/db/migration/V2__add_composite_key_index.sql
+++ uppick-main-service/src/main/resources/db/migration/V2__add_composite_key_index.sql
@@ -1,2 +1,2 @@
 CREATE INDEX idx_productid_sellat ON sell_detail (product_id ASC, sell_at DESC);
-CREATE INDEX idx_productid_purchaseat ON auction (product_id ASC, purchase_at DESC);
+CREATE INDEX idx_productid_purchaseat ON purchase_detail (product_id ASC, purchase_at DESC);
