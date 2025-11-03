58bea3c8c673b39843e3307db3ed6c4a8b1121fa
kimseokjun <k03s13j@naver.com>
2025-11-03 16:26:51 +0900

    feat : auction service에 replica 적용하기

---
+++ uppick-auction-service/src/main/java/org/oneog/uppick/auction/common/config/datasource/RoutingDataSource.java
@@ -0,0 +1,22 @@
+package org.oneog.uppick.auction.common.config.datasource;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+@Slf4j
+public class RoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        DataSourceType dataSourceType = DataSourceContextHolder.getDataSourceType();
+
+        // 명시적으로 설정되지 않았다면 MASTER 사용
+        if (dataSourceType == null) {
+            log.info(">>> DataSource 타입이 설정되지 않아 MASTER 사용");
+            return DataSourceType.MASTER;
+        }
+
+        log.info(">>> 현재 사용할 DataSource: {}", dataSourceType);
+        return dataSourceType;
+    }
+}
