e932286870f5892b9144a580491bca078f550dfd
kimseokjun <k03s13j@naver.com>
2025-11-04 17:34:57 +0900

    refactor : 잘못 넣은 코드 제거

--- uppick-auction-service/src/test/java/org/oneog/uppick/auction/support/restdocs/RestDocsConfig.java
+++ uppick-auction-service/src/test/java/org/oneog/uppick/auction/support/restdocs/RestDocsConfig.java
@@ -1,16 +1,10 @@
 package org.oneog.uppick.auction.support.restdocs;
 
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
-
-import java.nio.charset.StandardCharsets;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 
 import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.filter.CharacterEncodingFilter;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @TestConfiguration
 public class RestDocsConfig {
@@ -21,22 +15,6 @@ public class RestDocsConfig {
 		return configurer -> configurer.operationPreprocessors()
 			.withRequestDefaults(prettyPrint())
 			.withResponseDefaults(prettyPrint());
-	}
-
-	@Bean
-	CharacterEncodingFilter characterEncodingFilter() {
-		CharacterEncodingFilter filter = new CharacterEncodingFilter();
-		filter.setEncoding(StandardCharsets.UTF_8.name());
-		filter.setForceEncoding(true);
-		return filter;
-	}
-
-	@Bean
-	MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-		converter.setDefaultCharset(StandardCharsets.UTF_8);
-		converter.setObjectMapper(new ObjectMapper());
-		return converter;
 	}
 
 }
