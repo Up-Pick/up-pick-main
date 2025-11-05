2a5273d0a75a2da4d9e78d5cd1fc7198672abb95
baek_do_hyeon <ehgus6666@naver.com>
2025-11-06 02:30:37 +0900

    test: BidPriceItemReader 정상 데이터 읽기 성공

--- uppick-batch-service/src/test/java/org/oneog/uppick/batch/domain/bidprice/reader/BidPriceItemReaderTest.java
+++ uppick-batch-service/src/test/java/org/oneog/uppick/batch/domain/bidprice/reader/BidPriceItemReaderTest.java
@@ -1,12 +1,19 @@
 package org.oneog.uppick.batch.domain.bidprice.reader;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Set;
+
 import javax.sql.DataSource;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.oneog.uppick.batch.domain.bidprice.dto.BidPriceDto;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -35,6 +42,28 @@ class BidPriceItemReaderTest {
 
 		reader = new BidPriceItemReader(stringRedisTemplate, auctionDataSource);
 		ReflectionTestUtils.setField(reader, "auctionJdbcTemplate", jdbcTemplate);
+	}
+
+	@Test
+	@DisplayName("정상 데이터 읽기 성공")
+	void read_success() throws Exception {
+
+		// given
+		String key = "auction:1:current-bid-price";
+		when(stringRedisTemplate.keys("auction:*:current-bid-price")).thenReturn(Set.of(key));
+		when(stringRedisTemplate.opsForValue()).thenReturn(valueOperations);
+		when(valueOperations.get(key)).thenReturn("100000");
+		when(jdbcTemplate.queryForObject(anyString(), eq(Long.class), eq(1L))).thenReturn(10L);
+
+		// when
+		reader.init();
+		BidPriceDto result = reader.read();
+
+		// then
+		assertThat(result).isNotNull();
+		assertThat(result.getAuctionId()).isEqualTo(1L);
+		assertThat(result.getProductId()).isEqualTo(10L);
+		assertThat(result.getBidPrice()).isEqualTo(100000L);
 	}
 
 }
