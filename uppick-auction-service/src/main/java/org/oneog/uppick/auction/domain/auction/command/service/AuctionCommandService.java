817811b4fd71dde2c3d9010551bd2b203da3405d
abko <abko121115@gmail.com>
2025-11-05 02:38:09 +0000

    feat: 입찰 처리 중 락 획득 실패 및 예외 발생 시 처리 로직 추가

--- uppick-auction-service/src/main/java/org/oneog/uppick/auction/domain/auction/command/service/AuctionCommandService.java
+++ uppick-auction-service/src/main/java/org/oneog/uppick/auction/domain/auction/command/service/AuctionCommandService.java
@@ -3,6 +3,7 @@ package org.oneog.uppick.auction.domain.auction.command.service;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.exception.LockAcquisitionException;
 import org.oneog.uppick.auction.common.lock.LockManager;
 import org.oneog.uppick.auction.domain.auction.command.event.AuctionEventProducer;
 import org.oneog.uppick.auction.domain.auction.command.event.AuctionEventType;
@@ -10,6 +11,8 @@ import org.oneog.uppick.auction.domain.auction.command.event.BidPlacedEvent;
 import org.oneog.uppick.auction.domain.auction.command.model.dto.request.AuctionBidRequest;
 import org.oneog.uppick.auction.domain.auction.command.model.dto.request.BiddingResultDto;
 import org.oneog.uppick.auction.domain.auction.command.service.component.BiddingProcessor;
+import org.oneog.uppick.auction.domain.auction.common.exception.AuctionErrorCode;
+import org.oneog.uppick.common.exception.BusinessException;
 import org.springframework.stereotype.Service;
 
 import jakarta.validation.Valid;
@@ -35,15 +38,24 @@ public class AuctionCommandService {
 	public void bid(@Valid AuctionBidRequest request, long auctionId, long memberId) {
 
 		String lockKey = BIDDING_LOCK_KEY_PREFIX + auctionId;
-		BiddingResultDto result = lockManager.executeWithLock(lockKey, () -> biddingProcessor.process(request,
-			auctionId, memberId));
+		BiddingResultDto result;
+
+		try {
+			result = lockManager.executeWithLock(lockKey, () -> biddingProcessor.process(request, auctionId, memberId));
+		} catch (LockAcquisitionException e) {
+			log.error("입찰 처리 중 락 획득 실패", e);
+			throw new BusinessException(AuctionErrorCode.BID_FAILED_CAUSE_ACQUIRE_LOCK_FAILED);
+		} catch (Exception e) {
+			log.error("입찰 처리 중 예외 발생", e);
+			throw e;
+		}
 
 		BidPlacedEvent bidPlacedEvent = BidPlacedEvent.builder()
 			.sellerId(result.getSellerId())
 			.bidderId(memberId)
 			.auctionId(auctionId)
 			.biddingPrice(result.getBiddingPrice())
-			.eventId(UUID.randomUUID().toString() + "-" + auctionId)
+			.eventId(UUID.randomUUID() + "-" + auctionId)
 			.occurredAt(LocalDateTime.now())
 			.build();
 		auctionEventProducer.produce(AuctionEventType.BID_PLACED, bidPlacedEvent);
