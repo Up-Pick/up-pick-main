a79a47949a550ac6f9e9f9c7a4dbc26b1e405f41
abko <abko121115@gmail.com>
2025-11-17 06:48:59 +0000

    fix: 미씽 어노테이션 부착

--- uppick-main-service/src/main/java/org/oneog/uppick/domain/notification/query/controller/NotificationQueryController.java
+++ uppick-main-service/src/main/java/org/oneog/uppick/domain/notification/query/controller/NotificationQueryController.java
@@ -4,6 +4,7 @@ import org.oneog.uppick.common.dto.AuthMember;
 import org.oneog.uppick.common.dto.GlobalApiResponse;
 import org.oneog.uppick.domain.notification.query.service.NotificationQueryService;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +21,7 @@ public class NotificationQueryController {
     // 사용자가 읽지 않은 알림의 개수를 가져오는 엔드포인트
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/unread/count/me")
-    public GlobalApiResponse<Long> countUnreadNotificationsByMemberId(AuthMember authMember) {
+    public GlobalApiResponse<Long> countUnreadNotificationsByMemberId(@AuthenticationPrincipal AuthMember authMember) {
 
         long unreadCount = notificationQueryService.countUnreadNotificationsByMemberId(authMember.getMemberId());
         return GlobalApiResponse.ok(unreadCount);
